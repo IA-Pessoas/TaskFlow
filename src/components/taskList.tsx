d481fd179a5817803aa2a66ba877ee7bacec9cb0
Alan Sampaio <alansampaio62647@gmail.com>
2026-03-17 18:01:49 -0300

    Fase 6 : Filtros e busca

--- src/components/taskList.tsx
+++ src/components/taskList.tsx
@@ -1,8 +1,27 @@
 import { useTasksContext } from "../contexts/TasksContext";
+import { useDebounce } from "../hooks/useDebounce";
 import { TaskItem } from "./taskItem";
+import { useState, useMemo } from "react";
 
 export function TaskList(){ 
   const { tasks, toggleTask , deleteTask } = useTasksContext();
+
+  const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState<'all' | 'completed' | 'pending'>('all');
+  const debouncedSearch = useDebounce(search, 300);
+
+  const filteredtasks = useMemo(()=> {
+    return tasks
+    .filter(t => filter === 'all' || t.completed === (filter === 'completed'))
+    .filter(t => t.title.toLowerCase().includes(debouncedSearch.toLowerCase()));
+  },[tasks, filter, debouncedSearch])
+
+  const stats = useMemo(() => ({
+    total: tasks.length,
+    completd: tasks.filter(t => t.completed).length,
+    pending: tasks.filter(t => !t.completed).length,
+  }),[tasks]);
+
   return (
     <ul>
       {tasks.map((task) => (
